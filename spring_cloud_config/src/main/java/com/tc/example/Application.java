56a3b57fbc012f31818dc5e1ce51edd1f3487545
cai.tian <cai.tian@vipshop.com>
2017-07-24 11:07:03 +0800

    测试

--- spring_cloud_config/src/main/java/com/tc/example/Application.java
+++ spring_cloud_config/src/main/java/com/tc/example/Application.java
@@ -16,6 +16,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class Application {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
-		System.out.println("--配置中心--");
+		System.out.println("--配置中心1--");
 	}
 }
